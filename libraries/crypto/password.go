490dc08873a01ca10afcffff62c8c25064e1cf7b
donghquinn <ehdgus1524@gmail.com>
2024-04-21 23:45:14 +0900

    fixing encryption

--- libraries/crypto/password.go
+++ libraries/crypto/password.go
@@ -1,17 +1,29 @@
 package crypto
 
-import "golang.org/x/crypto/bcrypt"
+import (
+	"log"
+
+	"golang.org/x/crypto/bcrypt"
+)
 
 // 평문의 패스워드에서 단방향 해시를 생성한다
 func EncryptHashPassword(password string) (string, error) {
     bytes, err := bcrypt.GenerateFromPassword([]byte(password), 10)
-    return string(bytes), err
+    if err != nil {
+        log.Printf("[ENCODE] Encode Hash Password: %v", err)
+
+        return "", err
+    }
+    
+    return string(bytes), nil
 }
 
 // 패스워드 비교
 func PasswordCompare(hash, password string) (bool, error) {
     err := bcrypt.CompareHashAndPassword([]byte(hash), []byte(password))
     if err != nil {
+        log.Printf("[COMPARE] Compare Password Error: %v", err)
+        
         if err == bcrypt.ErrMismatchedHashAndPassword {
             // MEMO: err를 wrap 하여 상세를 전달하면 좋다
             return false, err
