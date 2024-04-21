7bba580c445c9a04a9c1b9f74f88769bdf3d8c6b
donghquinn <ehdgus1524@gmail.com>
2024-04-22 00:30:23 +0900

    wip: upload

--- routers/default.router.go
+++ routers/default.router.go
@@ -4,11 +4,13 @@ import (
 	"net/http"
 
 	"github.com/donghquinn/blog_back_go/controllers"
-	routers "github.com/donghquinn/blog_back_go/routers/users"
+	uploadRouters "github.com/donghquinn/blog_back_go/routers/upload"
+	userRouters "github.com/donghquinn/blog_back_go/routers/users"
 )
 
 func DefaultRouter(server *http.ServeMux) {
 	server.HandleFunc("GET /api", controllers.DefaultController)
 
-	routers.UserRouter(server)
+	userRouters.UserRouter(server)
+	uploadRouters.UploadImageController(server)
 }
