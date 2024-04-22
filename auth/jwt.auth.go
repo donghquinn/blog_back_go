1e9b6212673aa256759dd38440681f10540477e1
donghquinn <ehdgus1524@gmail.com>
2024-04-22 23:24:00 +0900

    log delete

--- auth/jwt.auth.go
+++ auth/jwt.auth.go
@@ -16,16 +16,6 @@ import (
 func CreateJwtToken(userId string, userEmail string, userStatus string) (string, error) {
 	globalConfig := configs.GlobalConfig
 
-	// block, _ := pem.Decode([]byte(globalConfig.JwtKey))
-
-	// privKey, parseErr := x509.ParsePKCS8PrivateKey(block.Bytes);
-
-	// if parseErr != nil {
-	// 	log.Printf("Parse PK Error: %v", parseErr)
-
-	// 	return "", parseErr
-	// }
-
 	jwtToken := jwt.New(jwt.SigningMethodHS256)
 
 	claims := jwtToken.Claims.(jwt.MapClaims)
@@ -43,42 +33,15 @@ func CreateJwtToken(userId string, userEmail string, userStatus string) (string,
 
 		return "", err
 	}
-	log.Printf("[JWT] Created Token: %s", token)
 	
 	return token, nil
 }
 
-
-// LoadPublicKey loads RSA public key from PEM format
-// func LoadPublicKey(publicKeyPEM string) (any, error) {
-//     block, _ := pem.Decode([]byte(publicKeyPEM))
-//     if block == nil {
-//         return nil, errors.New("failed to decode PEM block containing public key")
-//     }
-
-//     publicKey, err := x509.ParsePKIXPublicKey(block.Bytes)
-//     if err != nil {
-//         return nil, err
-//     }
-
-//     return publicKey, nil
-// }
-
 // JWT 키  검증
 func ValidateJwtToken(req *http.Request) (string, string, string, error) {
 	token := strings.Split(req.Header["Authorization"][0], "Bearer ")[1]
-	// var publicKey *rsa.PublicKey
 
 	globalConfig := configs.GlobalConfig
-
-	// 퍼블릭 키 검증
-	// publicKey, pubErr :=  LoadPublicKey(globalConfig.SecretKey)
-
-	// if pubErr != nil {
-	// 	log.Printf("[JWT] Pub Key Parse Error: %v", pubErr)
-
-	// 	return "", "", "", pubErr
-	// }
 
 	// JWT 토큰 파싱
 	parseToken, err := jwt.ParseWithClaims(token, &types.JwtInterface{}, func(token *jwt.Token) (interface{}, error) {
