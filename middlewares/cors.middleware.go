cdc3bd79c62d1c4e50a8a38681a4e4566c39a03f
donghquinn <ehdgus1524@gmail.com>
2024-06-19 23:06:33 +0900

    middlewares

--- middlewares/cors.middleware.go
+++ middlewares/cors.middleware.go
@@ -1,6 +1,7 @@
 package middlewares
 
 import (
+	"log"
 	"net/http"
 )
 
@@ -8,17 +9,18 @@ func CorsMiddlewares(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(res http.ResponseWriter, req *http.Request) {
 		res.Header().Set("Access-Control-Allow-Origin", "*")
 		res.Header().Set("Access-Control-Max-Age", "86400")
-		res.Header().Set("Access-Control-Allow-Methods", "POST, GET, OPTIONS")
+		res.Header().Set("Access-Control-Allow-Methods", "POST, GET")
 		res.Header().Set("Access-Control-Allow-Headers", "Content-Type, Authorization")
 		res.Header().Set("Access-Control-Allow-Credentials", "true")
 
-		// if req.Method == "OPTIONS" {
-		// 	log.Printf("Method Options")
 
-		// 	res.WriteHeader(http.StatusOK)
-		// 	return
-		// }
-		
+		// Handle preflight request
+	if req.Method == http.MethodOptions {
+		log.Printf("[MIDDLEWARE] Handling preflight request")
+		res.WriteHeader(http.StatusOK)
+		return
+	}
+
 		next.ServeHTTP(res, req)
 	})
 }
