3882c62f04d7448c7eb7e5a5f3dfc51b518b4fbd
donghquinn <ehdgus1524@gmail.com>
2024-06-20 21:25:58 +0900

    fixed crypt

--- test/decode.go
+++ test/decode.go
@@ -7,16 +7,20 @@ import (
 )
 
 func Test() {
-	receivedPw:=  "Z8CcFfKXIcOkTq1aBn/tew=="
-	dbPwd := "$2a$10$hKBV01zNJjuojmhUzdJ6z.2V/Ua4QLr/NxP86jUvj70M2jUSzM7h6"
-	
-	decoded, _ := crypt.DecryptString(receivedPw)
-	isMatch, err := crypt.PasswordCompare(dbPwd, decoded)
+	email := "ehdgus1524@gmail.com"
 
-	if err != nil {
-		log.Printf("ERR: %v", err)
-	}
-	log.Printf("[복호화된] email: %v", isMatch)
+	encoded , _ := crypt.EncryptString(email)
+	log.Println(encoded)
+	// receivedPw:=  "Z8CcFfKXIcOkTq1aBn/tew=="
+	// dbPwd := "$2a$10$hKBV01zNJjuojmhUzdJ6z.2V/Ua4QLr/NxP86jUvj70M2jUSzM7h6"
+	
+	// decoded, _ := crypt.DecryptString(receivedPw)
+	// isMatch, err := crypt.PasswordCompare(dbPwd, decoded)
+
+	// if err != nil {
+	// 	log.Printf("ERR: %v", err)
+	// }
+	// log.Printf("[복호화된] email: %v", isMatch)
 }
 
 // func generateRandomString(length int) (string, error) {
